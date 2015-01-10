71ece5091d834fd09e48e3b0c343373399924ea4
mikenewt <mikejnewt@gmail.com>
2015-01-09 19:53:39 -0800

    Added numberList array to hold post numbers in a list and deleted old function in Open class

--- content.py
+++ content.py
@@ -6,61 +6,33 @@ import re, csv
 
 
 class Open():
-	def compile_category(self):
-		f1 = open('livestrong_content_structure.csv', 'rU')
-
-		csv_f1 = csv.reader(f1, dialect=csv.excel)
-
-		# Split out each level of content tags into individual variables
-		category = []
-		subCategory = []
-		subSubCategory = []
-
-		# Make sure no duplicate entries get appended to lists
-		for row in csv_f1:
-			if row[0] not in category:
-				category.append(row[0])
-			else:
-				pass
-			
-			if row[1] not in subCategory:
-				subCategory.append(row[1])
-			else:
-				pass
-
-			if row[2] not in subSubCategory:
-				subSubCategory.append(row[2])
-			else:
-				pass
-
-		f1.close()
-
-		return category, subCategory, subSubCategory
 
 	def compile_content(self):
 	
 		# Vida content cards
-		f2 = open('content_cards.csv', 'rU')
+		f = open('content_cards_v1.csv', 'rU')
 
-		csv_f2 = csv.reader(f2, dialect=csv.excel)
-		csv_f2.next()
+		csv_f = csv.reader(f, dialect=csv.excel)
 
 		contentData = {}
-		contentList = []
+		numberList = []
+
+		for row in csv_f:
+			numberList.append(row[0])
 
 		# Title from Excel sheet column B becomes key and value containts content body and permalinks stored as strings in a list
-		for row in csv_f2:
-			contentData[row[0]] = [row[1], row[2], row[4]]
+		# for row in csv_f2:
+		# 	contentData[row[0]] = [row[1], row[2], row[4]]
 
 		# Take contentData and store it in list of dictionaries where each dict holds one title as key and contnt/permalink as value
-		for k, v in contentData.items():
-			data = {}
-			data[k] = v
-			contentList.append(data)
+		# for k, v in contentData.items():
+		# 	data = {}
+		# 	data[k] = v
+		# 	contentList.append(data)
 
 		f2.close()
 
-		return contentList
+		return contentData
 
 class Classifier():
 
