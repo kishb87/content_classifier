6f7b3bc573be62a64e2495fb5777dc8a9ef5065a
kishb87 <kish.bplm@gmail.com>
2015-01-11 18:44:48 -0800

    Remove float error messages and replace with 0

--- pandas_counter.py
+++ pandas_counter.py
@@ -4,19 +4,19 @@ df_xls = pd.read_excel('content_cards_v1.xls', index_col='Number', na_values='')
 
 def convert_and_count(str1, str2, category_name):
 	if type(str1) == float: 
-		return 'float in str1: ' + str(str1)
+		return 0
 	if type(str2) == float:
-		return 'float in ' + category_name + ': ' + str(str2)
+		return 0
 	try:
 		str1_converted = str1.encode('ascii', 'ignore')
 	except:
 		print ('Error in str1: ' + str(str1))
-		return -1
+		return 0
 	try:
 		str2_converted = str2.encode('ascii', 'ignore')
 	except:
 		print ('Error in str2: ' + str(str2))
-		return -1
+		return 0
 	return str.count(str1_converted, str2_converted)
 
 df_xls['category1_count'] = df_xls.apply(lambda row:convert_and_count(row['Article Content'], row['Category 1'], 'Category 1'), axis=1)
